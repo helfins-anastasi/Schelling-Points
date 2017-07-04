f00e6af9b3c07ae10d7e3e8067a439ed14c47d1c
Zach Helfinstein <zhelfinstein@umail.ucsb.edu>
2017-07-03 17:16:23 -0700

    Added models

--- scenario/models.py
+++ scenario/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Scenario(models.Model):
+    task = models.TextField()
+
+class Response(models.Model):
+    task = models.ForeignKey(Scenario)
+    value = models.TextField()
+    count = models.IntegerField()
